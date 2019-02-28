6b82320a247f34baf4ce6fc9f29840486badbac9
golammirza <golam.mirza@gmail.com>
2019-03-01 00:40:46 +0600

    Add user form

---
+++ NCBlog/NCBlog.Repository/User/IUsersRepository.cs
@@ -0,0 +1,11 @@
+﻿using NCBlog.Repository.BaseRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NCBlog.Repository.User
+{
+   public interface IUsersRepository : IBaseRepository<Model.Users>
+    {
+    }
+}
